ce2e53af06e453752cc11327fd4fe113978177e9
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-23 13:41:08 -0400

    feat: add function navigator

---
+++ src/navigation.js
@@ -0,0 +1,16 @@
+function navigator() {
+  console.log({ location });
+  if (location.hash.startsWith("#trends")) {
+    console.log("TRENDS");
+  } else if (location.hash.startsWith("#search=")) {
+    console.log("SEARCH");
+  } else if (location.hash.startsWith("#movie=")) {
+    console.log("MOVIE");
+  } else if (location.hash.startsWith("#category=")) {
+    console.log("Categories");
+  } else {
+    console.log("Home");
+  }
+
+  location.hash;
+}
